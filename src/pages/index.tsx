de5eafa631a83df958b5335f9a997f550542d0eb
Adrian Knapp <adrknapp@gmail.com>
2021-12-20 18:04:32 -0300

    Added searchBox ref

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Input from '../components/Form/Input';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type SignInFormData = {
   email: string;
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function Home() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
 
   const { errors } = formState; 
 
@@ -41,17 +50,13 @@ export default function Home() {
             name="email"
             label="Email" 
             error={errors.email}
-            {...register('email', {
-              required: 'E-Mail é obrigatório',
-            })} />
+            {...register('email')} />
           <Input
             type="password"
             name="password"
             label="Senha"
             error={errors.password}
-            {...register('password', {
-              required: 'Senha é obrigatória',
-            })} />
+            {...register('password')} />
         </Stack>
         <Button
           type="submit"
