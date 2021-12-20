92bd89081aa72abeb49a462b5760acaf4164bac3
Adrian Knapp <adrknapp@gmail.com>
2021-12-20 14:30:02 -0300

    Added SidebarDrawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import Logo from '../Header/Logo';
 import NotificationsNav from '../Header/NotificationsNav';
 import Profile from '../Header/Profile';
 import SearchBox from '../Header/SearchBox';
 
 export default function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,18 @@ export default function Header() {
       align="center"
       px="6"
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine}/>}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+        </IconButton>
+      )}
+
       <Logo />
 
       <SearchBox />
